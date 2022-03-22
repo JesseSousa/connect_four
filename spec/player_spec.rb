10083fa7ce836dce4fb3e7f29ce7c2da1a533407
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-03-22 11:57:25 -0300

    write tests for player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../lib/player'
+
+describe Player do
+  context 'when initializing first player' do
+    subject(:player_one) { described_class.new('Jesse') }
+
+    it 'has a yellow circle' do
+      expect(player_one.circle).to eq("\e[33m\u25cf\e[0m") 
+    end
+  end
+
+  context 'when initializing second player' do
+    subject(:player_two) { described_class.new('James') }
+
+    it 'has a red circle' do
+      expect(player_two.circle).to eq("\e[31m\u25cf\e[0m")
+    end
+  end
+
+  context 'when trying to create third player' do
+    it 'raises an exception' do
+      expect { described_class.new('Bob')}.to raise_error(an_instance_of(RuntimeError).and having_attributes(message: 'Only two players can be created'))
+    end
+  end
+end 
